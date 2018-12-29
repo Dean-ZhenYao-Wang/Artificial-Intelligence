9c103953e38866313c2dd5634c7960c1fce084bf
ZhenYaoWang <zhenyao.wang@wzy.ren>
2018-12-30 00:32:01 +0800

    1

---
+++ SpamOrHam/Doc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpamOrHam
+{
+    public enum DocType
+    {
+        Ham,
+        Spam,
+        UnknownLabel
+    }
+    public class ParseDoc
+    {
+        private static DocType ParseDocType(string label)
+        {
+            switch (label)
+            {
+                case "ham":
+                    return DocType.Ham;
+                case "spam":
+                    return DocType.Spam;
+                default:
+                    return DocType.UnknownLabel;
+            }
+        }
+
+        public static Doc ParseLine(string line)
+        {
+            var split = line.Split('\t');
+            var lable = ParseDocType(split[0]);
+            var message = split[1];
+            return new Doc { docType = lable, meessage = message };
+        }
+    }
+
+    public class Doc
+    {
+        public DocType docType { get; set; }
+        public string meessage { get; set; }
+    }
+}
--- SpamOrHam/Program.cs
+++ SpamOrHam/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,43 @@ namespace SpamOrHam
     {
         static void Main(string[] args)
         {
+            string path = @"D:\工作\资料\技术资料\人工智能\训练数据\SMSSpamCollection";
+            var dataset = File.ReadAllLines(path).ToList();
+            int spam = 0;
+            //dataset.ForEach(i =>
+            //{
+            //    var tt = ParseDoc.ParseLine(i);
+            //    Console.Write(tt.docType + "    ");
+            //    Console.WriteLine(tt.meessage);
+            //    DocType sh = PrimitiveClassifier(tt.meessage);
+            //    if (sh == DocType.Spam)
+            //        spam++;
+            //    Console.WriteLine(sh);
+            //});
+            //Console.WriteLine(spam);
+            //List<Doc> docs = new List<Doc>();
+            //dataset.ForEach(i =>
+            //{
+            //    docs.Add(ParseDoc.ParseLine(i));
+            //});
+            //var spamWithFREE = docs
+            //    .Where(m => m.docType == DocType.Spam)
+            //    .Where(m => m.meessage.Contains("FREE"))
+            //    .Count();
+            //Console.WriteLine("val spamWithFREE : int ="+spamWithFREE);
+            //var hamWithFREE = docs
+            //    .Where(m => m.docType == DocType.Ham)
+            //    .Where(m => m.meessage.Contains("FREE"))
+            //    .Count();
+            //Console.WriteLine("val hamWithFREE : int =" + hamWithFREE);
+            Console.ReadKey();
+        }
+        private static DocType PrimitiveClassifier(string sms)
+        {
+            if (sms.Contains("FREE"))
+                return DocType.Spam;
+            else
+                return DocType.Ham;
         }
     }
 }
